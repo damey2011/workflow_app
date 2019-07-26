6940058a6d76db3295d95cdc239827d02ccad781
Ogban Ugot <ogbanugot@gmail.com>
2019-07-26 18:36:43 +0100

    add heroku settings

--- workflow_app/settings.py
+++ workflow_app/settings.py
@@ -9,10 +9,14 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
+import django_heroku
 
 import os
 import cloudinary
 from decouple import config
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))

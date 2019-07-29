928f86062638e5651d496a61bfeb0695434f967f
Ogban Ugot <ogbanugot@gmail.com>
2019-07-29 05:27:26 +0100

    initial structure for process automation

--- process/views.py
+++ process/views.py
@@ -18,11 +18,13 @@ from guardian.utils import get_anonymous_user
 import cloudinary.uploader
 from rest_framework import viewsets
 from rest_framework import filters
+from django.conf import settings
+
 
 
 ##############################################################################################
 class ProcessList(generics.ListCreateAPIView):
-    '''Sample request: {'organization_id':'1','process_name':'StudentReg','description':'Student Registration'}'''
+    '''Sample request: {"organization":"1","process_name":"StudentReg","description":"Student Registration"}'''
     serializer_class = ProcessSerializer
     parser_classes = (MultiPartParser, FormParser,)
     filter_backends = [filters.SearchFilter]
@@ -47,7 +49,7 @@ class ProcessDetail(generics.RetrieveUpdateDestroyAPIView):
 
 ##############################################################################################
 class StageList(generics.ListCreateAPIView):
-    '''Sample request: {'process_id':'3',order':'1'}'''
+    '''Sample request: {"process":"3","order":"1"}'''
     serializer_class = StageSerializer
     parser_classes = (MultiPartParser, FormParser,)
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly)#permission for authenticated users and owner of Organization
@@ -70,7 +72,7 @@ class StageDetail(generics.RetrieveUpdateDestroyAPIView):
 
 ##############################################################################################
 class TaskList(generics.ListCreateAPIView):
-    '''Sample request: {'stage','document','form','groups':'2','users':'2'}'''
+    '''Sample request: {"stage":"1","document":"3","form":"2","groups":"2","users":"2"}'''
     serializer_class = TaskSerializer
     parser_classes = (MultiPartParser, FormParser,)
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly)#permission for authenticated users and owner of Organization
@@ -93,7 +95,7 @@ class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
 
 ##############################################################################################
 class DocumentList(generics.ListCreateAPIView):
-    '''Sample request: {'organization_id','file'}'''
+    '''Sample request: {"organization":"1","file":"cloudinary url"}'''
     serializer_class = DocumentSerializer
     parser_classes = (MultiPartParser, FormParser, FileUploadParser,)
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly)#permission for authenticated users and owner of Organization
@@ -105,14 +107,14 @@ class DocumentList(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         user = self.request.user.id if self.request.user.is_authenticated else get_anonymous_user() #handling unauthorized access
-        file = self.request.FILES['file']
-        if file:
-            result = cloudinary.uploader.upload(file, 
-                                                folder = "workflow801",
-                                                overwrite = "true", 
-                                                resource_type = "raw") #cloudinary file upload
-            file_url = result['url']  #store the public id cloudinary upload
-            serializer.save(user_id=user, file = file_url) #save the public id in db  
+        # file = self.request.FILES['file']
+        # if file:
+        #     result = cloudinary.uploader.upload(file, 
+        #                                         folder = "workflow801",
+        #                                         overwrite = "true", 
+        #                                         resource_type = "raw") #cloudinary file upload
+        #     file_url = result['secure_url']  #store the public id cloudinary upload
+        serializer.save(user_id=user) 
 
 class DocumentDetail(generics.RetrieveUpdateDestroyAPIView):
     '''Retrieve, modify or delete Document.'''
@@ -123,7 +125,7 @@ class DocumentDetail(generics.RetrieveUpdateDestroyAPIView):
 
 ##################################################################################################
 class FormList(generics.ListCreateAPIView):
-    '''Sample request: {'organization_id','form_name','description','fields'}'''
+    '''Sample request: {"organization":"1","form_name":"1","description":"Form description","fields":"{form html elements}"}'''
     serializer_class = FormSerializer
     parser_classes = (MultiPartParser, FormParser,)
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly)#permission for authenticated users and owner of Organization
@@ -146,7 +148,7 @@ class FormDetail(generics.RetrieveUpdateDestroyAPIView):
 
 ####################################################################################################
 class FormresponseList(generics.ListCreateAPIView):
-    '''Sample request: {'form_id','response'}'''
+    '''Sample request: {"form_id":"2","response":"Response"}'''
     serializer_class = FormresponseSerializer
     parser_classes = (MultiPartParser, FormParser,)
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly)#permission for authenticated users and owner of Organization
@@ -187,3 +189,58 @@ def processflow(request, format=None):
                                                 last_name=last_name)
             return Response(serializer.data, status=201)                     
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)    
+
+###############################################################################################################
+@api_view(['POST'])
+def processflow(request, format=None):
+    '''
+    Sample request: {
+        "id": 1,
+        "user_id":3,
+        "stage": 4,
+        "document": null,
+        "form": 1,
+        "groups": null,
+        "users": null,
+        "isComplete": false
+    }
+    '''
+    #set isComplete in task True
+    task_id = request.data.get("id")
+    task = Tasks.objects.get(pk=task_id)
+    task.isComplete = True
+    task.save()
+    #Get stage_id of the task 
+    stage_id = request.data.get("stage")
+    stage = Stage.objects.get(pk = stage_id)
+    #Get process_id of the stage
+    process_id = stage.process.id   
+    #Get all the tasks in stage
+    tasks_in_stage = Tasks.objects.filter(stage=stage_id)
+    numberofTasks = len(tasks_in_stage)
+    completeTasks = 0
+    #check how many tasks are isComplete
+    for task in tasks_in_stage:
+        if task.isComplete == True:
+            completeTasks += 1
+    
+    if completeTasks == numberofTasks:
+        #if all the tasks in the stage are complete, set the stage to complete
+        stage.isComplete = True
+        stage.save()
+        # check if all the stages in the process are complete
+        stages_in_process = Stage.objects.filter(process = process_id)
+        numberofStages = len(stages_in_process)
+        completeStage = 0
+        for stage in stages_in_process:
+            if stage.isComplete == True:
+                completeStage += 1
+        #if all the stages in the process are complete, set process to complete
+        if completeStage == numberofStages:                
+            process_data = Process.objects.get(pk=process_id)
+            process_data.isComplete = True
+            process_data.save()
+        Response(status=status.HTTP_200_OK)   
+
+    else:
+        Response(status=status.HTTP_200_OK)   

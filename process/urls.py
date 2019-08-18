8bdc6f3917741776f32f7f958e59d47299adac48
Ogban Ugot <ogbanugot@gmail.com>
2019-08-18 16:08:03 +0100

    edit task url

--- process/urls.py
+++ process/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     url(r'^stage/$', views.StageList.as_view()),  # List Stage
     url(r'^stage/(?P<pk>[0-9]+)/$', views.StageDetail.as_view(), ),  # Particular stage
 
-    url(r'^stage/task$', views.TaskList.as_view()),  # List Task
+    url(r'^stage/task/$', views.TaskList.as_view()),  # List Task
     url(r'^stage/task/(?P<pk>[0-9]+)/$', views.TaskDetail.as_view(), ),  # Particular Task
 
     url(r'^form/$', views.FormList.as_view()),  # list Form
